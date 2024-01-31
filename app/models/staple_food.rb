a090f7c30eddf5f27cab7e1c78129578048dbd0b
Takumi19980801 <takunomi1998@gmail.com>
2024-01-31 20:12:36 +0900

    栄養素確認ページ作成(主食まで)

--- app/models/staple_food.rb
+++ app/models/staple_food.rb
@@ -1,9 +1,9 @@
 class StapleFood < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '米',   },
-    { id: 3, name: 'パン' },
-    { id: 4, name: '麺' },
-    { id: 5, name: 'スパゲティ' },
+    { id: 2, name: '米', ns1: 51.9, ns2: 3, ns3:0.3},
+    { id: 3, name: 'パン', ns1: 29.9, ns2: 5.3, ns3:2.4},
+    { id: 4, name: '麺', ns1: 67.16, ns2: 11.27, ns3:1.38},
+    { id: 5, name: 'パスタ', ns1: 80.5, ns2: 14.5, ns3:2.25},
   ]
   end
