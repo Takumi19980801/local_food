9eea091ee58afb581fa767cd6b8f0068fe217ae4
Takumi19980801 <takunomi1998@gmail.com>
2024-03-01 18:41:41 +0900

    個数セレクトボックス作成ボタン追加

---
+++ app/javascript/SBC_create2.js
@@ -0,0 +1,20 @@
+// ボタン要素を取得
+const createSelectButton = document.getElementById('createSelect_count_Button2');
+
+// ボタンがクリックされたときの処理
+createSelectButton.addEventListener('click', function() {
+  // 新しいセレクト要素を作成
+  const newSelect = document.createElement('select');
+
+  // オプションを追加
+  const options = ['選択してください', 'オプション1', 'オプション2', 'オプション3'];
+  options.forEach(option => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = option;
+    newSelect.appendChild(optionElement);
+  });
+
+  // 新しいセレクトボックスをコンテナに追加
+  const selectContainer = document.getElementById('select_count_Container2');
+  selectContainer.appendChild(newSelect);
+});
